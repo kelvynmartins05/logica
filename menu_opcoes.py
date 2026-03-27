9452437efd4417ec4b23aa70f46e694993e06c8b
Kelvyn de Morais Martins <kelvyn.martins@edu.senai.br>
2026-03-27 08:23:36 -0300

    Create menu_opcoes.py

---
+++ menu_opcoes.py
@@ -0,0 +1,15 @@
+print("1 - Opção 1\n2 - Opção 2\n3 - Opção 3\n4 - Sair")
+opcao = input("Digite uma opção: ")
+
+if opcao == '1':
+    print("Você selecionou a opção 1")
+elif opcao == '2':
+    print("Você selecionou a opção 2")
+elif opcao == '3':
+    print("Você selecionou a opção 3")
+elif opcao == '4':
+    print("Você selecionou sair")
+else:
+    print("Opção inválida!!!")
+
+print("Fim do programa!")
